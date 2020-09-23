1fa7d5aa2976b05924afce23a00db01ca8cc5e5f
AhRaul <ralchik@yandex.ru>
2020-09-23 15:04:50 +0300

    live data simple lesson 2
    https://startandroid.ru/ru/courses/architecture-components/27-course/architecture-components/525-urok-2-livedata.html

--- app/src/main/java/com/example/livedata/MainActivity.java
+++ app/src/main/java/com/example/livedata/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.livedata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.livedata.app.DataController;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        LiveData<String> liveData = DataController.getInstance().getData();
+
+        liveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String value) {
+                textView.setText(value);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/livedata/app/DataController.java
+++ app/src/main/java/com/example/livedata/app/DataController.java
@@ -2,9 +2,13 @@ package com.example.livedata.app;
 
 import android.app.Application;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 public class DataController extends Application {
 
     private static DataController instance;
+    private MutableLiveData<String> liveData = new MutableLiveData<>();
 
     @Override
     public void onCreate() {
@@ -14,5 +18,20 @@ public class DataController extends Application {
 
     public static DataController getInstance() {
         return instance;
+    }
+
+    public LiveData<String> getData() {
+        return liveData;
+    }
+
+    /**
+     *     Метод setValue должен быть вызван из UI потока.
+     *     Для обновления данных из других потоков используйте метод postValue.
+     *     Он перенаправит вызов в UI поток.
+     *     Соответственно, подписчики всегда будут получать значения в основном потоке.
+     * @param newValue
+     */
+    public void setData(String newValue) {
+        this.liveData.setValue(newValue);
     }
 }
