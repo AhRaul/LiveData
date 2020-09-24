1be442aab19ddce67ef06aaa699995a99ffb9968
AhRaul <ralchik@yandex.ru>
2020-09-24 15:13:40 +0300

    live data simple lesson 2 add action button

--- app/src/main/java/com/example/livedata/MainActivity.java
+++ app/src/main/java/com/example/livedata/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.livedata.app.DataController;
@@ -12,6 +14,8 @@ import com.example.livedata.app.DataController;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    Button bChangeData;
+    DataController dataController = DataController.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        bChangeData = findViewById(R.id.b_change_data);
 
         LiveData<String> liveData = DataController.getInstance().getData();
 
@@ -26,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChanged(String value) {
                 textView.setText(value);
+            }
+        });
+
+        bChangeData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataController.setData("новая инфа");
             }
         });
     }
